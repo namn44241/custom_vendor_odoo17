16183fe4aa401fb89665d8f8013ae1fcd5309fa0
namn44241 <nguyenvandoaaaaa@gmail.com>
2025-05-23 23:05:47 +0700

    [FIX] không cập nhật sale order nếu product quantity < 0

--- demo/models/sale_order_line.py
+++ demo/models/sale_order_line.py
@@ -1,6 +1,16 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
     warehouse_id = fields.Many2one('stock.warehouse', string='Kho hàng') 
+
+    @api.constrains('product_uom_qty')
+    def _check_positive_quantity(self):
+        for line in self:
+            # Chỉ kiểm tra với hàng hoá thực (type='product')
+            if line.product_id.type == 'product' and line.product_uom_qty <= 0:
+                raise ValidationError(_(
+                    "Số lượng cho sản phẩm '%s' phải lớn hơn 0."
+                ) % line.product_id.display_name) 
