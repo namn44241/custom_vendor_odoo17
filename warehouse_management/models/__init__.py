eff650dbd84f0397a4dc0d81b858936951ccb6dd
namn44241 <nguyenvandoaaaaa@gmail.com>
2025-05-25 00:02:42 +0700

    [ADD] final 2

--- warehouse_management/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import res_partner
-from . import purchase_order
-from . import stock_scrap
-from . import stock_warehouse
-from . import sale_order
-from . import sale_order_line 
--- warehouse_management/models/sale_order_line.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-    
-    warehouse_id = fields.Many2one('stock.warehouse', string='Kho hàng') 
